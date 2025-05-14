344a71d6e661a808515e34864cd614c5bf555dcf
Veronica <lily7sph4@gmail.com>
2025-05-14 21:23:28 +0300

    более удачные попытки отрисовки рычага

--- levermodule.cpp
+++ levermodule.cpp
@@ -3,7 +3,14 @@
 
 LeverModule::LeverModule(sf::Vector2f newOrigin, float newSide, std::string newSerial, sf::Font newFont) : BaseModule(newOrigin, newSide, newSerial, newFont)
 {
+    circle.setRadius(side * 0.065f);
+    circle.setOrigin(circle.getGeometricCenter());
+    circle.setPosition(origin + sf::Vector2(side / 2.0f, side / 4.0f));
+    circle.setFillColor(sf::Color::Red);
 
+    stick.setSize({side * 0.04f, side * 0.3f});
+    stick.setOrigin({stick.getSize().x / 2.f, 0.f});
+    stick.setPosition(circle.getPosition());
 }
 
 void LeverModule::process(sf::RenderWindow *window, int time)
@@ -12,28 +19,16 @@ void LeverModule::process(sf::RenderWindow *window, int time)
         && sf::Mouse::getPosition(* window).x > circle.getPosition().x - circle.getRadius() && sf::Mouse::getPosition(* window).x < circle.getPosition().x + circle.getRadius()
         && sf::Mouse::getPosition(* window).y > circle.getPosition().y - circle.getRadius() && sf::Mouse::getPosition(* window).y < stick.getPosition().y + stick.getSize().y)
     {
-        circle.setFillColor(sf::Color::Blue);
-        circle.setPosition(circle.getPosition() + sf::Vector2(0.f, stick.getSize().y + 2.f * circle.getRadius()));
+        stick.setPosition(stick.getPosition() + sf::Vector2(0.f, stick.getSize().y));
+
+        circle.setPosition(stick.getPosition() + sf::Vector2(0.f, stick.getSize().y));
     }
-    window->draw(circle);
 }
 
 void LeverModule::render(sf::RenderWindow *window)
 {
-    circle.setRadius(side * 0.08f);
-    circle.setOrigin(circle.getGeometricCenter());
-    circle.setPosition(origin + sf::Vector2(side / 2.0f, side / 2.0f));
-    circle.setFillColor(sf::Color::Red);
-
-    stick.setSize({side * 0.04f, side * 0.3f});
-    stick.setOrigin({stick.getSize().x / 2.f, 0.f});
-    stick.setPosition(circle.getPosition() + sf::Vector2(0.f, circle.getRadius()));
-
     window->draw(stick);
-    if (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
-    {
-        window->draw(circle);
-    }
+    window->draw(circle);
 
     std::array vertices =
         {
