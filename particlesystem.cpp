5518b784b2ecd94cb4296b61b4c3036810feacc5
Veronica <lily7sph4@gmail.com>
2025-05-24 22:24:13 +0300

    созданы кнопки играть и выход, окно под QR код, точки рассеиваются от мышки

---
+++ particlesystem.cpp
@@ -0,0 +1,46 @@
+#include "particlesystem.h"
+
+
+void ParticleSystem::setEmitter(sf::Vector2f position)
+{
+    m_emitter = position;
+}
+
+void ParticleSystem::update(sf::Time elapsed)
+{
+    for (std::size_t i = 0; i < m_particles.size(); ++i)
+    {
+        Particle& p = m_particles[i];
+        p.lifetime -= elapsed;
+
+        if (p.lifetime <= sf::Time::Zero)
+            resetParticle(i);
+
+        m_vertices[i].position += p.velocity * elapsed.asSeconds();
+
+        float ratio           = p.lifetime.asSeconds() / m_lifetime.asSeconds();
+        m_vertices[i].color.a = static_cast<std::uint8_t>(ratio * 255);
+    }
+}
+
+void ParticleSystem::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+
+    states.texture = nullptr;
+
+    target.draw(m_vertices, states);
+}
+
+void ParticleSystem::resetParticle(std::size_t index)
+{
+    static std::random_device rd;
+    static std::mt19937       rng(rd());
+
+    const sf::Angle angle       = sf::degrees(std::uniform_real_distribution(0.f, 360.f)(rng));
+    const float     speed       = std::uniform_real_distribution(20.f, 40.f)(rng);
+    m_particles[index].velocity = sf::Vector2f(speed, angle);
+    m_particles[index].lifetime = sf::milliseconds(std::uniform_int_distribution(1000, 3000)(rng));
+
+    m_vertices[index].position = m_emitter;
+}
---
+++ particlesystem.h
@@ -0,0 +1,35 @@
+#ifndef PARTICLESYSTEM_H
+#define PARTICLESYSTEM_H
+#include <SFML/Graphics.hpp>
+#include <random>
+
+
+class ParticleSystem : public sf::Drawable, public sf::Transformable
+{
+public:
+    ParticleSystem(unsigned int count) : m_particles(count), m_vertices(sf::PrimitiveType::Points, count)
+    {
+    }
+
+    void setEmitter(sf::Vector2f position);
+
+    void update(sf::Time elapsed);
+
+private:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+    struct Particle
+    {
+        sf::Vector2f velocity;
+        sf::Time     lifetime;
+    };
+
+    void resetParticle(std::size_t index);
+
+    std::vector<Particle> m_particles;
+    sf::VertexArray       m_vertices;
+    sf::Time              m_lifetime{sf::seconds(6)};
+    sf::Vector2f          m_emitter;
+};
+
+#endif // PARTICLESYSTEM_H
